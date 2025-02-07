58530b2bbfe5b340b052d449e10545f260019227
Sina Salahshour <sina.salahshour.32@gmail.com>
2025-02-07 09:22:00 +0330

    feat: accept input.txt, change output formatting

--- mah.py
+++ mah.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import re
 import sys
+from pathlib import Path
 
 from actions import register_actions
 from code_interpreter import run_code
@@ -15,14 +16,19 @@ USAGE_HELP_MESSGE = """Usage:
     mah build\t <input file>\t\t# to see the program instructions"""
 
 
-def print_code_block(ir: IRGenerator):
+def print_code_block(ir: IRGenerator, should_save_to_file=False):
+    code_block = "\n"
     for index, code in enumerate(ir.sstack[:400]):
         if not code:
             break
-        print(
-            f"{index}:\t\t{'\t|\t'.join(map(lambda x: '-' if x is None else str(x),code))}"
-        )
-        print("\t  " + "- " * 32)
+        code_block += f"{index}:\t{'|'.join(map(lambda x: ' '.center(9) if x is None else str(x).center(9),code))}\n"
+        code_block += ("\t " + "-" * 36) + "\n"
+
+    if not should_save_to_file:
+        print(code_block)
+    else:
+        with open("output.txt", "w") as f:
+            f.write(code_block)
 
 
 def read_file(file_name):
@@ -64,12 +70,17 @@ def find_error_line(input_str: str, pos: int):
 
 
 def main():
+    should_save_to_file = False
     if len(sys.argv) == 3:
         command = sys.argv[1]
         file_name = sys.argv[2]
     elif len(sys.argv) == 2:
         command = "run"
         file_name = sys.argv[1]
+    elif len(sys.argv) == 1 and Path("input.txt"):
+        command = "build"
+        file_name = "input.txt"
+        should_save_to_file = True
     else:
         print(USAGE_HELP_MESSGE)
         exit(-1)
@@ -79,7 +90,7 @@ def main():
         match command:
             case "build":
                 ir = generate_code(file_str)
-                print_code_block(ir)
+                print_code_block(ir, should_save_to_file=should_save_to_file)
             case "run":
                 ir = generate_code(file_str)
                 run_code(ir.sstack[:400])
